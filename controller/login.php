be3ffe731c4a83cc4c4e0e60755169baef5ad155
egheose <icepriince@gmail.com>
2014-07-10 17:55:13 +0100

    moved data to login controller

---
+++ controller/login.php
@@ -0,0 +1,43 @@
+<?php
+//Displaying views etc
+
+class login
+{
+    //private $nam=array('hm'=>'home','ab'=>'about us');
+    private $data;
+    //constructor (its doing nothing for now)
+    function __construct(){}
+
+    //Functions
+    function load($url)
+    {
+        include($url);
+    }
+
+    //Set and Get data
+    function set_data()
+    {
+        //$this->data[$name]=htmlentities($value,ENT_QUOTES);
+        $this->data = array(
+            'Home',
+            'About',
+            'Contact',
+        );
+    }
+    function get_data()
+    {
+        if(isset($this->data))
+        {
+            echo '<ul class="navbar-left nav navbar-nav">';
+            echo '<li><a class="navbar-brand" href="/">COG AFRICA</a></li>';
+            for($i=0;$i<count($this->data);$i++){
+                print_r('<li><a class="navbar-brand" href="controller/' .$this->data[$i] .'" >' .$this->data[$i] .'</a></li>');
+            }
+            echo '</ul>';
+        }
+        else
+        {
+            return "Inside Else Block";
+        }
+    }
+}
--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
-include("./model/m_template.php");
-$Template = new Template();
+$url = explode('/',$_GET['url']);
+
+print_r($url);
+
+require 'controller/' .$url[0] .'.php' ;
+$controller = new $url[0];
+$Template = new login();
 $Template->set_data();
 $Template->load("./view/v_login.php");
