4aa90aa838fb05015b286cfb316d04bc048f8db3
egheose <icepriince@gmail.com>
2014-07-19 22:12:07 +0100

    Set home directory

--- controller/registration.php
+++ controller/registration.php
@@ -28,7 +28,7 @@ class registration
         if(isset($this->data))
         {
             echo '<ul class="navbar-left nav navbar-nav">';
-            echo '<li><a class="navbar-brand" href="/">COG AFRICA</a></li>';
+            echo '<li><a class="navbar-brand" href="/cogAfrica/">COG AFRICA</a></li>';
             for($i=0;$i<count($this->data);$i++){
                 print_r('<li><a class="navbar-brand" href="controller/' .$this->data[$i] .'" >' .$this->data[$i] .'</a></li>');
             }
