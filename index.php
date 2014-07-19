10ae9d6e73adb9d3edc783e9961708229842f699
egheose <icepriince@gmail.com>
2014-07-19 16:47:57 +0100

    troubleshooting

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 error_reporting(0);
 $url = $_GET['url'];
 
+if (empty($url)) {
+    // no data passed by get
+    $url = 'login';
+}
 //print_r($url) ;
 $file = './controller/' .$url .'.php';
 //require './controller/login.php';
@@ -12,4 +16,4 @@ if(file_exists($file))
     //$controller->set_data();
     $controller->load("./view/" .$url .".php");
 }
-else{ echo 'File not found';  }
+else{ echo 'File not found'; echo $file;  }
